f3e29fda1387a7d3b769feee592ee7200ea3de3f
Michelle Glass <mglass@Michelles-MacBook-Air.local>
2020-04-08 10:44:59 -0500

    adding information to const and arrays

---
+++ app.js
@@ -0,0 +1,5 @@
+const name = 'Michelle Glass';
+const field = 'Bootcamp Student';
+const description = 'An artistc, excitable Disney princess in disguise; trying to learn to code, with the help of her compaion, the coding corgi.'
+let interists = ['Painting', 'Sewing -mostly quilting', 'Crafts', 'Disney', 'Corgis', 'Cats', 'Animals in general', 'Coding', 'Cooking' ];
+let skills = ['Teaching', 'Quilting', 'Shen Lung Kung Fu', 'High Animal Kin', 'Painting','Pinterest', 'Creativity','Kitty wrangling', ]
