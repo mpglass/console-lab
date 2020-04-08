93e60392dbd60d8842fe14f4a8edbe1e743d7d6a
Michelle Glass <mglass@Michelles-MacBook-Air.local>
2020-04-08 15:19:06 -0500

    added job objects ready to write displayPosition function

--- app.js
+++ app.js
@@ -2,7 +2,33 @@ const name = 'Michelle Glass';
 const field = 'Bootcamp Student';
 const dscrp = 'An artistc, excitable Disney princess in disguise; trying to learn to code, with the help of her compaion, the coding corgi.';
 let interests = ['Painting', 'Sewing -mostly quilting', 'Crafts', 'Disney', 'Corgis', 'Cats', 'Animals in general', 'Coding', 'Cooking' ];
-let jobs = ['Finder of "a book" at the place of a million books', 'Pricer of DvDs and CDs at the place with the buys are best', 'Wrangler of MTG and YGO! players and seller of comics at the place where the crisis started', 'Parts messenger at the luckyest auto parts store', 'Teacher of crafts and maker of messes at the only dedicated leasure services center in the state, with a singular populace'];
+
+let jobBook = {
+    title: 'Tracker' ,  
+    where: 'The place of a million books', 
+    whatDo: 'Finder of "a book" '
+};
+let jobMedia = {
+    title: 'Shopkeeper',
+    where: 'The place where the buys are best', 
+    whatDo: 'Pricer of DvDs and CDs'
+}; 
+let jobComic = {
+    title: 'Archon',
+    where: 'the place where the destrucion started',
+    whatDo: 'Wrangler of the MTG and YGO! players, seller of comics, and reciever of all items'
+}  
+let jobAuto = {
+    title: 'Parts Courier',
+    where: 'The luckyest auto parts store',
+    whatDo: 'Conveyer of parts to the ones who fix' 
+};
+let jobTRC = {
+    title: 'Manager of Messes', 
+    where: 'The center of recreation',
+    whatDo: 'Teacher of crafts and maker of messes' 
+};
+
 let skills = ['Teaching', 'Quilting', 'Shen Lung Kung Fu', 'High Animal Kin','Ran over 200,000 console logs in less than an hour', 'Painting','Pinterest', 'Creativity','Kitty wrangling', 'Walt level Disney Fan', 'Maker of rainbows-in my hair', 'Leads a Pop army'];
 
 const up = name.toUpperCase()
